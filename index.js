2f05d630303a2a0e1761cc2875ecca7c253dd4ef
kasprzyk <piotkas1@gmail.com>
2021-01-05 16:26:31 +0100

    testing OAuth

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ passport.use(
   )
 );
 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
