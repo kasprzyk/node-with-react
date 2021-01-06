e236107a084e8038fcd9fcdfbee609b95a418f78
kasprzyk <piotkas1@gmail.com>
2021-01-06 12:39:38 +0100

    mongoose model classes

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('./services/passport');
 const keys = require('./config/keys');
+require('./models/User');
 
 const app = express();
 
 require('./routes/authRoutes')(app);
-mongoose.connect(keys.mongoURI, {
+
+const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+};
+mongoose.connect(keys.mongoURI, options);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
