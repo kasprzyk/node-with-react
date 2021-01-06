efc2d7f40fe12e654a2dcb50e767983e9bc2cc5b
kasprzyk <piotkas1@gmail.com>
2021-01-06 11:40:50 +0100

    connecting with mongoose

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
+const keys = require('./config/keys');
 
 const app = express();
 
 require('./routes/authRoutes')(app);
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
