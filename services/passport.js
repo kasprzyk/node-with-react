0633cd5dece9837d7c9c8a33ac3cf968e28dba6d
kasprzyk <piotkas1@gmail.com>
2021-01-06 19:12:28 +0100

    mongoose queries

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,14 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          console.log('user exists');
+        } else {
+          new User({ googleId: profile.id }).save();
+        }
+      });
+
       // console.log(accessToken);
       // console.log(refreshToken);
       // console.log(profile);
