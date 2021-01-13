926965e1844f61e24644cfbf2d6b55fed4c396e3
kasprzyk <piotkas1@gmail.com>
2021-01-13 18:16:20 +0100

    router setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const Survey = () => <h2>Survey</h2>;
+const Landing = () => <h2>Landing</h2>;
+
 const App = () => {
   return <div>Hello</div>;
 };
