622fa686bde9cc4e9331d6f371f4b51802fd91c9
kasprzyk <piotkas1@gmail.com>
2021-01-16 00:38:55 +0100

    Always visible components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 const Header = () => <h2>Header</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const Survey = () => <h2>Survey</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
@@ -11,8 +12,10 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
+          <Header />
           <Route exact path='/' component={Landing} />
-          <Route path='/surveys' component={Dashboard} />
+          <Route exact path='/surveys' component={Dashboard} />
+          <Route path='/surveys/new' component={SurveyNew} />
         </div>
       </BrowserRouter>
     </div>
