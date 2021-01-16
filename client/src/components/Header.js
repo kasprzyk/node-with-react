f22626c7971d37247f4b53e11a5bfcac20988cc0
kasprzyk <piotkas1@gmail.com>
2021-01-16 13:52:58 +0100

    Header design

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className='nav-wrapper'>
+          <a className='left brand-logo'>Emaily</a>
+          <ul className='right'>
+            <li>
+              <a>Login with google</a>
+            </li>
+            <li></li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
