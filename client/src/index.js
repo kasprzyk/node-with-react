41ca3849313a95d2f0c1610cb28eac618daabef7
kasprzyk <piotkas1@gmail.com>
2021-01-25 22:09:13 +0100

    thunk, axios

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
+import reduxThunk from 'redux-thunk';
 
 const store = createStore(reducers, {}, applyMiddleware());
 ReactDOM.render(
