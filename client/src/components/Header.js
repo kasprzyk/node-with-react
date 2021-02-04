47e8905a25a52ed35fa88ffae90879418920448c
kasprzyk <piotkas1@gmail.com>
2021-02-04 10:21:55 +0100

    fix

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -27,7 +27,7 @@ class Header extends Component {
       <nav>
         <div className='nav-wrapper'>
           <Link
-            to={this.props.user ? '/surveys' : '/'}
+            to={this.props.auth ? '/surveys' : '/'}
             className='left brand-logo'
           >
             Emaily
